23fd8612bb9c386b2bf1ec554e5feff40e6c7e6d
EzechielKiregha <ekiregha@gmail.com>
2024-03-07 23:54:50 +0200

    FileStorage class (serialization & deserialization)

---
+++ models/engine/file_storage.py
@@ -0,0 +1,34 @@
+import json
+import os
+
+
+class FileStorage:
+    """
+    This class serves as an Object-Relation Mapping interface to store and retrieve objects using JSON format.
+    """
+
+    __objects = {}
+    __file_path = 'file.json'
+
+    def all(self):
+        """Return all instances stored"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Stores a new Object"""
+        key = f"{type(obj).__name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """Serializes objects stored and persists them in a file"""
+        serialized = {key: value.to_dict() for key, value in FileStorage.__objects.items()}
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            json.dump(serialized, f)
+
+    def reload(self):
+        """reload method that update __objects dictionary 'deserialized from JSON file' """
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as js_f:
+                for key, obj in json.loads(js_f.read()).items():
+                    obj = eval(obj['__class__'])(**obj)
+                    FileStorage.__objects[key] = obj
