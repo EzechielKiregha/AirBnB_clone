e13e3b6b43633e402a10d29abccc2a5845a8cbdf
collinzy0451 <tamunosakicollinsjacob@gmail.com>
2024-03-08 21:13:26 +0000

    File contains the class state

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This file contain the class State"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+	"""
+		State class that inherits from BaseModel class
+		"""
+		name = ""
+
