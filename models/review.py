cffd093a6f1dd10493eb1f8e3e70949eaa7c1f36
collinzy0451 <tamunosakicollinsjacob@gmail.com>
2024-03-08 21:16:17 +0000

    File contains the class review

---
+++ models/review.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This file contain the class Review"""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+	"""
+		Review class that inherits from BaseModel class
+		"""
+		place_id = ""
+		user_id = ""
+		text = ""
