38b1742f178e343b016e8002f9f7ddf0d81dafb5
collinzy0451 <tamunosakicollinsjacob@gmail.com>
2024-03-08 21:10:40 +0000

    A file that contains the class user

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This file contain the class user"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+	"""
+		User class that inherits from BaseModel class
+		"""
+		email = ""
+		password = ""
+		first_name = ""
+		last_name = ""
